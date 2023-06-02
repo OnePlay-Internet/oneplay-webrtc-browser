b789499c14781cbc34af359d258ee89977c8e372
BurnedOx666 <mainak@oneplay.in>
2023-06-02 17:24:38 +0530

    setting up base path webplay

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 module.exports = {
+    basePath: "/webplay",
     webpack(config, { isServer }) {
         config.module.rules.push({
             test: /\.svg$/i,
             issuer: /\.[jt]sx?$/,
             use: ["@svgr/webpack"],
         });
-        const prefix = config.assetPrefix ?? config.basePath ?? "";
         config.module.rules.push({
             test: /\.mp4$/,
             use: [
                 {
                     loader: "file-loader",
                     options: {
-                        publicPath: `${prefix}/_next/static/media/`,
+                        publicPath: "/webplay/_next/static/media/",
                         outputPath: `${isServer ? "../" : ""}static/media/`,
                         name: "[name].[hash].[ext]",
                     },
@@ -21,7 +21,7 @@ module.exports = {
         });
         return config;
     },
-	experimental: {
-		appDir: true,
-	}
+    experimental: {
+        appDir: true,
+    },
 };
